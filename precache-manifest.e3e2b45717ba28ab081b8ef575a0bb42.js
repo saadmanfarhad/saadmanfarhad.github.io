e86cb506c03490f9094550f58c698bf83e88dd60
Saadman Farhad <saadmanheavy@gmail.com>
2020-03-20 21:43:44 +0600

    Pushing to github pages

---
+++ precache-manifest.e3e2b45717ba28ab081b8ef575a0bb42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72df65e70a0d77cbb3e905ed59a36d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a036d1aebedc35bbee1",
+    "url": "/static/css/main.16050289.chunk.css"
+  },
+  {
+    "revision": "ce799ad34d28dbcba965",
+    "url": "/static/js/2.51e348ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51e348ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a036d1aebedc35bbee1",
+    "url": "/static/js/main.64d4dfd0.chunk.js"
+  },
+  {
+    "revision": "58eaef1b8e950042ec0e",
+    "url": "/static/js/runtime-main.a963a316.js"
+  },
+  {
+    "revision": "6cd18bc0983f441a1b55d38408a271a7",
+    "url": "/static/media/wave-bot.6cd18bc0.png"
+  },
+  {
+    "revision": "ba75e0c9c558e20911df32aa3cc7a5bd",
+    "url": "/static/media/wave-mid.ba75e0c9.png"
+  },
+  {
+    "revision": "05d52988a72cb1da374bc1690f060224",
+    "url": "/static/media/wave-top.05d52988.png"
+  }
+]);
